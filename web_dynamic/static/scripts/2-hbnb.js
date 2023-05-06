61da9b338ead1d0258385be90e0cef53e753de11
oneharry <harryofficialchidi@yahoo.com>
2023-05-06 22:23:31 +0000

    advanced tasks

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -6,15 +6,3 @@ $.get(url), (res) => {
     $('div#api_status').removeClass('available');
   }
 };
-
-$.ajax({
-  url: 'http://0.0.0.0:5001/api/v1/places_search/',
-  type: POST,
-  contentType: 'application/json',
-  data: '{}',
-  success: (res) => {
-    $('section.places').append(res.map(place => {
-      return ('<article> <div class="title"> <h2>' + place.name + '</h2><div class="price_by_night">' + '$' + place.price_by_night + '</div></div> <div class="information"> <div class="max_guest"> <i class="fa fa -users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"> <i class="fa fa -users fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"> <i class="fa fa -users fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom </div></div> <div class="user"></div><div class="description">' + '$' + place.description + '</div></article>');
-    }));
-  }
-});
--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -11,7 +11,7 @@ $.ajax({
   url: 'http://0.0.0.0:5001/api/v1/places_search/',
   type: POST,
   contentType: 'application/json',
-  data: '{}',
+  data: JSON.stringify({ amenities: Object.keys(amenityIds) }),
   success: (res) => {
     $('section.places').append(res.map(place => {
       return ('<article> <div class="title"> <h2>' + place.name + '</h2><div class="price_by_night">' + '$' + place.price_by_night + '</div></div> <div class="information"> <div class="max_guest"> <i class="fa fa -users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div><div class="number_rooms"> <i class="fa fa -users fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"> <i class="fa fa -users fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom </div></div> <div class="user"></div><div class="description">' + '$' + place.description + '</div></article>');
